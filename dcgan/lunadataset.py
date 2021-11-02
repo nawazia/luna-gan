9e645c1ab481f5d2277651175f8d84503138ed18
Ibrahim Nawaz <ibrahim.nawaz1@icloud.com>
2021-11-02 13:57:29 +0000

    Updated lunadataset.py

--- dcgan/lunadataset.py
+++ dcgan/lunadataset.py
@@ -1,15 +1,25 @@
 import torch
 from torch.utils.data import Dataset
+import SimpleITK as sitk
+import numpy as np
+import matplotlib.pylab as plt
 
 
-
-
+def load_itk_image(filename):
+    itkimage = sitk.ReadImage(filename)
+    numpyImage = sitk.GetArrayFromImage(itkimage)
+    #print(f'array type: {type(numpyImage)}')
+    #print(f'shape of array: {np.shape(numpyImage)}')
+    numpyOrigin = np.array(list(reversed(itkimage.GetOrigin())))
+    numpySpacing = np.array(list(reversed(itkimage.GetSpacing())))
+     
+    return numpyImage, numpyOrigin, numpySpacing
 
 class LunaDataset(Dataset):
-    def __init__(self,
+    def __init__(self, file
     
     ):
-        #insert code
+        lungCT, _, _ = load_itk_image(file)
         ...
 
     def __len__(self):
