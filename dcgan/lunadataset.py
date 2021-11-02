b54d6cabd6f6590edbf91d57f161741e1b02ca94
Ibrahim Nawaz <ibrahim.nawaz1@icloud.com>
2021-11-02 13:43:54 +0000

    Added lunadataset Dataset class

---
+++ dcgan/lunadataset.py
@@ -0,0 +1,20 @@
+import torch
+from torch.utils.data import Dataset
+
+
+
+
+
+class LunaDataset(Dataset):
+    def __init__(self,
+    
+    ):
+        #insert code
+        ...
+
+    def __len__(self):
+        return ...
+
+
+    def __getitem__(self, idx):
+        return ...
